a21d6c71e7b92b07c4134894318e3fac07722f97
Fernando Leano <fernandoleano4@gmail.com>
2022-02-26 16:50:04 -0500

    Drumkit app

---
+++ script.js
@@ -0,0 +1,43 @@
+// const { Howl } = require("./sound");
+
+const sounds = new Howl({
+    "src": [
+        "./sound/drums.webm",
+        "./sound/drums.mp3"
+      ],
+        "sprite": {
+          "clap": [
+            0,
+            734.2630385487529
+          ],
+          "closed-hihat": [
+            2000,
+            445.94104308390035
+          ],
+          "crash": [
+            4000,
+            1978.6848072562354
+          ],
+          "kick": [
+            7000,
+            553.0839002267571
+          ],
+          "open-hihat": [
+            9000,
+            962.7664399092968
+          ],
+          "snare": [
+            11000,
+            354.48979591836684
+            ]
+    }
+});
+
+const drumkit = document.querySelector(".drumkit");
+
+drumkit.addEventListener('click', () => {
+    if (event.target.classList.contains('pad')) {
+        let drumSound = event.target.dataset.sound;
+        sounds.play(drumSound);
+    }
+});
